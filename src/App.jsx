78cd9c09cc701f307f6b27d2bc70a7ad7b0e0481
abdninesix <mohabd1995@gmail.com>
2025-06-27 14:56:19 +0500

    New

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Contact from "./components/contact/contact"
+import Hero from "./components/hero/hero"
+import Portfolio from "./components/portfolio/portfolio"
+import Services from "./components/services/services"
+
+const App = () => {
+  return (
+    <div className='container'>
+      <section id="#hero">
+        <Hero />
+      </section>
+
+      <section id="#services">
+        <Services />
+      </section>
+
+      <section id="#portfolio">
+        <Portfolio />
+      </section>
+
+      <section id="#contact">
+        <Contact />
+      </section>
+    </div>
+  )
+}
+
+export default App
