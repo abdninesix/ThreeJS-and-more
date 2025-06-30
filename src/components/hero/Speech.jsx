86670bf14baca2bc9d9cd79c879bff9261a1f4d6
abdninesix <mohabd1995@gmail.com>
2025-06-30 14:50:45 +0500

    Updated

--- src/components/hero/Speech.jsx
+++ src/components/hero/Speech.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { TypeAnimation } from 'react-type-animation'
 
 const Speech = () => {
   return (
     <div className='bubbleContainer'>
-        <div className='bubble'>Text</div>
+        <div className='bubble'>
+          <TypeAnimation
+          sequence={[
+            1000,
+            "Random text Random text",
+            1000,
+            "More random text",
+            1000,
+          ]}
+          wrapper='span'
+          speed={40}
+          deletionSpeed={80}
+          style={{}}
+          repeat={Infinity}
+          />
+        </div>
         <img src="/man.png" alt="bubble" />
     </div>
   )
